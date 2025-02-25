38c81867f9061233036d081a0feabe7e19168c29
Adam Carpenter <adamcarpenter86@gmail.com>
2025-02-25 15:45:48 -0700

    Use oracles endpoint to oracles list

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -1,9 +1,21 @@
-import type { ReadChain } from '$lib/@types/types'
-import { BN_CHAINS_ENDPOINT } from '../../constants'
+import type { ReadChain, OracleChain } from '$lib/@types/types'
+import { BN_CHAINS_ENDPOINT, BN_ORACLE_CHAINS_ENDPOINT } from '../../constants'
 
 export async function fetchChains(): Promise<ReadChain[] | undefined> {
 	try {
 		const response = await fetch(BN_CHAINS_ENDPOINT)
+		if (!response.ok) throw new Error('Failed to fetch chains')
+		const data = await response.json()
+		return data
+	} catch (error) {
+		console.error('Error fetching chains:', error)
+		return undefined
+	}
+}
+
+export async function fetchOracleChains(): Promise<OracleChain[] | undefined> {
+	try {
+		const response = await fetch(BN_ORACLE_CHAINS_ENDPOINT)
 		if (!response.ok) throw new Error('Failed to fetch chains')
 		const data = await response.json()
 		return data
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,37 +1,64 @@
-import type { EstimationData } from "./@types/types"
+import {
+	defaultOracleMainnetChain,
+	evmV2ContractTypValues,
+	mainnetV2ContractTypValues,
+	utxoV2ContractTypValues
+} from '../constants'
+import type { EstimationData, OracleChain } from './@types/types'
 
 // Prepare estimation data
 export const createEstimationObject = (pred: EstimationData): EstimationData => {
-  const { Q70, Q80, Q90, Q95, Q99, precision, height, timestamp, chainid } = pred
-  return {
-    Q70: {
-      gasPrice: Q70.gasPrice,
-      maxPriorityFeePerGas: Q70.maxPriorityFeePerGas,
-      maxFeePerGas: Q70.maxFeePerGas
-    },
-    Q80: {
-      gasPrice: Q80.gasPrice,
-      maxPriorityFeePerGas: Q80.maxPriorityFeePerGas,
-      maxFeePerGas: Q80.maxFeePerGas
-    },
-    Q90: {
-      gasPrice: Q90.gasPrice,
-      maxPriorityFeePerGas: Q90.maxPriorityFeePerGas,
-      maxFeePerGas: Q90.maxFeePerGas
-    },
-    Q95: {
-      gasPrice: Q95.gasPrice,
-      maxPriorityFeePerGas: Q95.maxPriorityFeePerGas,
-      maxFeePerGas: Q95.maxFeePerGas
-    },
-    Q99: {
-      gasPrice: Q99.gasPrice,
-      maxPriorityFeePerGas: Q99.maxPriorityFeePerGas,
-      maxFeePerGas: Q99.maxFeePerGas
-    },
-    precision: precision,
-    height: BigInt(height),
-    timestamp: BigInt(timestamp),
-    chainid: BigInt(chainid)
-  }
+	const { Q70, Q80, Q90, Q95, Q99, precision, height, timestamp, chainid } = pred
+	return {
+		Q70: {
+			gasPrice: Q70.gasPrice,
+			maxPriorityFeePerGas: Q70.maxPriorityFeePerGas,
+			maxFeePerGas: Q70.maxFeePerGas
+		},
+		Q80: {
+			gasPrice: Q80.gasPrice,
+			maxPriorityFeePerGas: Q80.maxPriorityFeePerGas,
+			maxFeePerGas: Q80.maxFeePerGas
+		},
+		Q90: {
+			gasPrice: Q90.gasPrice,
+			maxPriorityFeePerGas: Q90.maxPriorityFeePerGas,
+			maxFeePerGas: Q90.maxFeePerGas
+		},
+		Q95: {
+			gasPrice: Q95.gasPrice,
+			maxPriorityFeePerGas: Q95.maxPriorityFeePerGas,
+			maxFeePerGas: Q95.maxFeePerGas
+		},
+		Q99: {
+			gasPrice: Q99.gasPrice,
+			maxPriorityFeePerGas: Q99.maxPriorityFeePerGas,
+			maxFeePerGas: Q99.maxFeePerGas
+		},
+		precision: precision,
+		height: BigInt(height),
+		timestamp: BigInt(timestamp),
+		chainid: BigInt(chainid)
+	}
+}
+
+export const getOracleByChainId = (
+	chains: OracleChain[],
+	chainId: number | undefined
+): OracleChain => {
+	return chains.find((chain) => chain.chainId === chainId) ?? defaultOracleMainnetChain
+}
+
+export const getTypValuesByArch = (arch: number, chainId?: number): number[] => {
+	switch (arch) {
+		case 1:
+			return utxoV2ContractTypValues
+		case 2:
+			if (chainId === 1) {
+				return mainnetV2ContractTypValues
+			}
+			return evmV2ContractTypValues
+		default:
+			return []
+	}
 }
